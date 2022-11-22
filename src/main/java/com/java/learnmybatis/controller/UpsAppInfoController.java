73726e1ea334dd567d46509bc173bb8b807ee228
yuanhang08 <yuanhang08@meituan.com>
2022-11-22 17:34:49 +0800

    使用hibernate-validator校验参数

--- src/main/java/com/java/learnmybatis/controller/UpsAppInfoController.java
+++ src/main/java/com/java/learnmybatis/controller/UpsAppInfoController.java
@@ -9,9 +9,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,25 +28,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("/ups/app")
 @Api(tags = "UPSAppKey管理")
+@Validated
 public class UpsAppInfoController {
     @Resource
     private UpsAppInfoService upsAppInfoService;
 
     @RequestMapping(value = "/info/{id}", method = RequestMethod.GET)
     @ApiOperation(value = "通过id获取appkey信息")
-    public ResultVo getUpsAppInfoById(@PathVariable @ApiParam("appkey的id") Integer id) {
+    public ResultVo getUpsAppInfoById(@Valid @PathVariable @Min(value = 1, message = "id不能小于1") @ApiParam("appkey的id") Integer id) {
         UpsAppInfo upsAppInfo = upsAppInfoService.getUpsAppInfoById(id);
-        return ResultVo.success(upsAppInfo);
+        return upsAppInfo == null ? ResultVo.fail() : ResultVo.success(upsAppInfo);
     }
 
     @RequestMapping(value = "/batch-get/info", method = RequestMethod.GET)
     @ApiOperation(value = "通过id批量获取appkey信息")
-    public ResultVo batchGetUpsAppInfo(@RequestParam("ids") @ApiParam("appkey的id列表") List<Integer> ids) {
+    public ResultVo batchGetUpsAppInfo(@Valid @RequestParam("ids") @NotEmpty(message = "id列表不能为空") @ApiParam("appkey的id列表") List<Integer> ids) {
         List<UpsAppInfo> upsApps = upsAppInfoService.getUpsAppInfoByIds(ids);
-        if (CollectionUtils.isEmpty(upsApps)) {
-            return ResultVo.fail();
-        }
-        return ResultVo.success(upsApps);
+        return CollectionUtils.isEmpty(upsApps) ? ResultVo.fail() : ResultVo.success(upsApps);
     }
 
     @RequestMapping(value = "/feed-back-status", method = RequestMethod.GET)
